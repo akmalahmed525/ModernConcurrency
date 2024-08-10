e0e20cf26747be39f5f174624f157764adbe7791
Akmal Ahmed <akmal.ahmed@itelasoft.com>
2024-08-10 19:53:31 +0530

    Initial Commit

---
+++ ModernConcurrency/ModernConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernConcurrencyApp.swift
+//  ModernConcurrency
+//
+//  Created by Akmal on 10/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct ModernConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
